1e7bc46d95a5d515d814052cb9bbba94662ed0e7
bufferOverflowsOP <bufferOverflowsOP@gmail.com>
2024-04-08 18:50:23 +0000

    Latex added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
+import './App.scss';
+
 const App = () => {
+  const [text, setText] = useState("");
   return (
     <>
       <p className="title">Math Forum!</p>
       <p>This is a place dedicated to mathematics research and communication!</p>
+      <textarea placeholder="Enter text with LaTeX" rows={6} 
+      style={{ width: 420 }} value={text} 
+      onChange={(event) => {
+        setText(event.target.value);
+      }}
+      />
+      <br />
+      <span>
+        <p className="preview">Latex previewer: </p>
+        <Latex>{text ? text : "$\\text{Nothing entered.}$"}</Latex>
+      </span>
     </>
   )
 }
