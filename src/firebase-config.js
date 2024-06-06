ad5340a84b4384d431b733fef3787eb35c509ab9
charleswang0221 <charleswang0221@gmail.com>
2024-06-06 15:55:48 +0000

    Added default color, and improved navbar

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,15 +4,15 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   apiKey: "AIzaSyBDAlJ62ypuPpAKnWh7Cq_2-SzoGE4Td4w",
   authDomain: "mathematics-forum-62862.firebaseapp.com",
   projectId: "mathematics-forum-62862",
   storageBucket: "mathematics-forum-62862.appspot.com",
   messagingSenderId: "262622979097",
   appId: "1:262622979097:web:a2a26fee8bdffa8400f688",
-  measurementId: "G-27R5BW4WXP",
+  measurementId: "G-27R5BW4WXP"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
